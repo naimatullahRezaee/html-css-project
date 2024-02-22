9df3745d0ce834f3833d53cbf89360e6745bf699
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-22 09:59:23 +0430

    add colse and open btn menu

--- main.js
+++ main.js
@@ -23,3 +23,21 @@ faqs.forEach((faq) => {
 		}
 	});
 });
+
+// show/hide nav menu buttons
+
+const menu = document.querySelector(".nav__menu");
+const openBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+openBtn.addEventListener("click", () => {
+	menu.style.display = "flex";
+	closeBtn.style.display = "inline-block";
+	openBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+	menu.style.display = "none";
+	closeBtn.style.display = "none";
+	openBtn.style.display = "inline-block";
+});
